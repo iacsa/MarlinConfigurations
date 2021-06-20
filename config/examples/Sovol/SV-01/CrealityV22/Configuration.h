6f233ec2139e934a0e145ae99b5475d5cb313948
Matthias Iacsa <mail@matthias.iacsa.de>
2021-06-20 11:30:35 +0200

    Increase wait time & decrease window for M109 For more consistent bed & hotend heating

--- config/examples/Sovol/SV-01/CrealityV22/Configuration.h
+++ config/examples/Sovol/SV-01/CrealityV22/Configuration.h
@@ -484,11 +484,11 @@
 //#define MAX31865_SENSOR_OHMS_1      100
 //#define MAX31865_CALIBRATION_OHMS_1 430
 
-#define TEMP_RESIDENCY_TIME         10  // (seconds) Time to wait for hotend to "settle" in M109
-#define TEMP_WINDOW                  2  // (°C) Temperature proximity for the "temperature reached" timer
-#define TEMP_HYSTERESIS              4  // (°C) Temperature proximity considered "close enough" to the target
+#define TEMP_RESIDENCY_TIME         20  // (seconds) Time to wait for hotend to "settle" in M109
+#define TEMP_WINDOW                  1  // (°C) Temperature proximity for the "temperature reached" timer
+#define TEMP_HYSTERESIS              3  // (°C) Temperature proximity considered "close enough" to the target
 
-#define TEMP_BED_RESIDENCY_TIME     10  // (seconds) Time to wait for bed to "settle" in M190
+#define TEMP_BED_RESIDENCY_TIME     20  // (seconds) Time to wait for bed to "settle" in M190
 #define TEMP_BED_WINDOW              1  // (°C) Temperature proximity for the "temperature reached" timer
 #define TEMP_BED_HYSTERESIS          3  // (°C) Temperature proximity considered "close enough" to the target
 
